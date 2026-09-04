1569c1daed649fd996fe1131e4810a62f85c06cb
JulesRoques <julesroques@icloud.com>
2026-09-04 11:19:27 +0200

    sandbox

--- backend/src/sandbox.py
+++ backend/src/sandbox.py
@@ -1,3 +1,4 @@
+from business_object.game_mode.game_mode_factory import GameModeFactory
 from business_object.game_mode.dice_mode import DiceMode
 from business_object.game_mode.coin_flip_mode import CoinFlipMode
 from business_object.player import Player
@@ -15,3 +16,7 @@ print("---")
 
 for _ in range(10):
     print(coin.play(p1, p2, choice="heads"))
+
+print(GameModeFactory.get_mode("dice"))
+print(GameModeFactory.get_mode("coinflip"))
+GameModeFactory.get_mode("poker")   # doit lever une ValueError
