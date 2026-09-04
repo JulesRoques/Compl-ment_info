b518446ad0fedff3bc0a8ea9954ef048d02398d6
JulesRoques <julesroques@icloud.com>
2026-09-04 11:59:36 +0200

    feat: extraction du calcul Elo dans ScoringStrategy

--- backend/src/sandbox.py
+++ backend/src/sandbox.py
@@ -1,22 +1,13 @@
 from business_object.game_mode.game_mode_factory import GameModeFactory
-from business_object.game_mode.dice_mode import DiceMode
-from business_object.game_mode.coin_flip_mode import CoinFlipMode
 from business_object.player import Player
+from business_object.scoring_strategy import ScoringStrategy
 
 p1 = Player(username="Jacky", elo=1500, email="jacky@ensai.fr")
 p2 = Player(username="Jackie", elo=1500, email="jackie@ensai.fr")
 
-dice = DiceMode()
-coin = CoinFlipMode()
+game = GameModeFactory.get_mode("coinflip").play(p1, p2, choice="heads")
+print(game)
+print("avant :", p1.elo, p2.elo)
 
-for _ in range(10):
-    print(dice.play(p1, p2))
-
-print("---")
-
-for _ in range(10):
-    print(coin.play(p1, p2, choice="heads"))
-
-print(GameModeFactory.get_mode("dice"))
-print(GameModeFactory.get_mode("coinflip"))
-GameModeFactory.get_mode("poker")   # doit lever une ValueError
+ScoringStrategy.update_player_ratings(game)
+print("après :", p1.elo, p2.elo)
